ba1ea6ec16d1af10e0efb5f2ba96da0c77430853
coderedge <coderedge@gmail.com>
2024-08-13 18:30:42 +0100

    it stay just to manahe city

--- js/main.js
+++ js/main.js
@@ -1,16 +1,33 @@
 
-
-
 document.querySelector('button').addEventListener('click',function(){
     const city = document.querySelector('input').value;
     lien = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&appid=56aad21df110a70cd45fff6163a62c75'
     window.fetch(lien)
     .then(response => response.json())
-    .then(responseJson => console.log(responseJson));
+    .then(responseJson => {console.log(responseJson);
+        printer(responseJson);
+    });
+
 })
 
-function meteo() {
+function printer(data) {
+    const humidity = data.main.humidity;
+    document.querySelector('#hum').innerHTML = humidity;
 
+    const temp = data.main.temp;
+    document.querySelector('#temp').innerHTML = temp;
+
+    const speed = data.wind.speed;
+    document.querySelector('#vent').innerHTML = speed;
+
+    const icon = data.weather['0'].icon;
+    document.querySelector('#icon').src = 'http://openweathermap.org/img/w/'+icon+'.png';
+    console.log(icon);
+
+    const desc = data.weather['0'].description;
+    document.querySelector('#desc').innerHTML = desc;
     
 }
+
+
 
