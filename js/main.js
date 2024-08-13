ab20bd99ceeb3791c0900513d9a378b109e9a13d
coderedge <coderedge@gmail.com>
2024-08-13 19:08:17 +0100

    https

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 document.querySelector('button').addEventListener('click',function(){
     const city = document.querySelector('input').value;
-    lien = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&appid=56aad21df110a70cd45fff6163a62c75'
+    lien = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&appid=56aad21df110a70cd45fff6163a62c75'
     window.fetch(lien)
     .then(response => response.json())
     .then(responseJson => {console.log(responseJson);
